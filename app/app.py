ee01a10c53d8db088d05c1be08783dd4845f722d
anbuchelva <anbuchelva@gmail.com>
2025-01-01 15:55:03 +0530

    move app.py to add dir

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -13,6 +12,3 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
         return templates.TemplateResponse("index.html", {"request": request})
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8678)
