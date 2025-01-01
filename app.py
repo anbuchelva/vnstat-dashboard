c23532ff7f539be5cea98229dc27d91becc407dd
anbuchelva <anbuchelva@gmail.com>
2025-01-01 15:18:39 +0530

    add home route with templates

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # Initialize FastAPI app
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+        return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8678)
